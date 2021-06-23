0778ad38fa55f5b5488a2e0a5e232c73ce6ee495
BuildTools <unconfigured@null.spigotmc.org>
2021-06-23 16:16:40 +0700

    Removing ThymeLeaf in favor of more JSON orientated return.

--- src/main/java/com/net/SprintOne/controller/MainController.java
+++ src/main/java/com/net/SprintOne/controller/MainController.java
@@ -3,20 +3,22 @@ package com.net.SprintOne.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class MainController {
 
-    @GetMapping("/login")
-    public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @GetMapping("/date")
+    public String getDate() {
         Date d1 = new Date();
         DateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         return sdf.format(d1);
     }
-
 }
