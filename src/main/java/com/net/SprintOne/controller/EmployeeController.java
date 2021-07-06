b457509f4d09b16a6e29b3326e5f7e5b51a6c7b3
AnhH271 <derpyderpsquid@gmail.com>
2021-07-06 12:09:21 +0700

    Update controller

--- src/main/java/com/net/SprintOne/controller/EmployeeController.java
+++ src/main/java/com/net/SprintOne/controller/EmployeeController.java
@@ -16,6 +16,9 @@ import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/list")
 public class EmployeeController{
 
     @Autowired
